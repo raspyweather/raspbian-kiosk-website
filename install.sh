cff7b6a3e0ff5c71e005378d6557b4d3ee2f2815
raspyweather <raspyweather@gmail.com>
2018-09-03 17:26:51 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+echo "this script installs chromium and creates an autostart for the default installation of raspbian..."
+sudo apt install -y chromium-browser ttf-mscorefonts-installer unclutter x11-server-utils pv hdparm htop
+echo "Installation of packages finished. Now downloading newest version of the startscript"
+curl -o /home/pi/.config/lxsession/LXDE-pi https://raw.githubusercontent.com/raspyweather/raspbian-kiosk-website/master/start.sh
+echo "now setting up autostart config"
+
+echo "@xscreensaver -no-splash" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "@point-rpi" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "@unclutter" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "@xset s off" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "@xset s noblank" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "@xset -dpms" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+echo "@sh /home/pi/.config/lxsession/LXDE-pi/start.sh" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+
+echo "done, restarting."
+sudo shutdown -r now
