75c6935b2abdecc9631060825930fe20f659b92c
raspyweather <raspyweather@gmail.com>
2019-03-04 11:28:58 +0100

    added screensaver protection

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# disable screen saver
+xset s off
+xset s noblank
+xset -dpms
+
 website=$(head -n 1 /home/pi/Desktop/url.txt) 
 zoom=$(head -n 2 /home/pi/Desktop/url.txt | tail -n 1) 
 /usr/bin/chromium-browser --incognito --start-maximized --kiosk --force-device-scale-factor=$zoom $website
